8dc004e3fa12fa53f57d14ed519dcd413636de41
Osvaldo Brignoni <hello@obrignoni.com>
2016-10-27 22:24:42 -0500

    add WebhookClassHandler transformation

--- src/Handler/WebhookClassHandler.php
+++ src/Handler/WebhookClassHandler.php
@@ -1,6 +1,35 @@
 <?php namespace Oz\Webhooks\Handler;
 
-class WebhookClassHandler
+use Oz\Webhooks\Contract\WebhookClassHandlerInterface;
+use Oz\Webhooks\Contract\WebhooksInterface;
+
+class WebhookClassHandler implements WebhookClassHandlerInterface
 {
+
+    /**
+     * @var WebhooksInterface
+     */
+    protected $webhooks;
+
+    /**
+     * EventClassHandler constructor.
+     * @param WebhooksInterface $webhooks
+     * @internal param string $webhookName
+     */
+    public function __construct(WebhooksInterface $webhooks)
+    {
+        $this->webhooks = $webhooks;
+    }
+
+    /**
+     * Transform the webhook name to a class.
+     *
+     * @param string $name
+     * @return string
+     */
+    public function getWebhookClass($name)
+    {
+        return $this->webhooks->getWebhooksNamespace() . '\\' . studly_case($name);
+    }
 
 }
