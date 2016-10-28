6311f1f0b217d3958fd57b9366d47de23cb81c1d
Osvaldo Brignoni <hello@obrignoni.com>
2016-10-27 22:26:11 -0500

    add WebhooksInterface methods

--- src/Contract/WebhooksInterface.php
+++ src/Contract/WebhooksInterface.php
@@ -10,4 +10,19 @@ interface WebhooksInterface
      */
     public function getEventsNamespace();
 
+    /**
+     * Get the webhooks request namespace.
+     *
+     * @return string
+     */
+    public function getWebhooksNamespace();
+
+    /**
+     * Call a webhook by name.
+     *
+     * @param string $name
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function call($name);
+
 }
